14153b4dd421d2d903440d78aff22dc7618a4ac0
CiaranZ <czheng739@connect.hkust-gz.edu.cn>
2024-02-29 04:17:13 +0800

    modfied PDO

--- test/linux/simple_test/simple_test.c
+++ test/linux/simple_test/simple_test.c
@@ -57,9 +57,31 @@ struct TorqueIn {
     uint16 status_word; //6041
 };
 
-struct TorqueOut *target;
-struct TorqueIn *val;
+typedef struct DSTRUCT_OUT     // pdo
+{                              // 1c12
+    int32_t target_position;   // 0x607a
+    int32_t target_velocity;   // 0x60ff
+    int16_t max_torque;        // 0x6072
+    uint16_t control_word;     // 0x6040 pdo domain 0x1604
+    int16_t target_torque;     // 0x6071
+    uint8_t mode_of_operation; // 0x6060
+} out_ELMOt;
 
+typedef struct DSTRUCT_IN           // pdo
+{                                   // 1c13
+    int32_t position_actual;        // 0x6064
+    int32_t position_follow_err;    // 0x60f4
+    int16_t torque_actual;          // 0x6077
+    uint16_t status_word;           // 0x6041
+    uint8_t mode_of_operation_disp; //
+    int32_t velocity_actual;        // 0x606C
+} in_ELMOt;
+
+// struct TorqueOut *target;
+// struct TorqueIn *val;
+
+out_ELMOt *target;
+in_ELMOt *val;
 void* ecatprint()
 {
    //  int slave;
@@ -93,11 +115,89 @@ void* ecatprint()
         int __ret = ec_SDOwrite(slaveId, idx, sub, FALSE, __s, &buf, EC_TIMEOUTRXM);  \
         printf("Slave: %d - Write at 0x%04x:%d => wkc: %d; data: 0x%.*x\t{%s}\n", slaveId, idx, sub, __ret, __s, (unsigned int)buf, comment);    \
     }
+
+#define WRITECA(slaveId, idx, sub, buf, value, comment) \
+    {   \
+        int __s = sizeof(buf);  \
+        buf = value;    \
+        int __ret = ec_SDOwrite(slaveId, idx, sub, TRUE, __s, &buf, EC_TIMEOUTRXM);  \
+        printf("Slave: %d - Write at 0x%04x:%d => wkc: %d; data: 0x%.*x\t{%s}\n", slaveId, idx, sub, __ret, __s, (unsigned int)buf, comment);    \
+    }
+
 #define CHECKERROR(slaveId)   \
 {   \
     ec_readstate();\
     printf("EC> \"%s\" %x - %x [%s] \n", (char*)ec_elist2string(), ec_slave[slaveId].state, ec_slave[slaveId].ALstatuscode, (char*)ec_ALstatuscode2string(ec_slave[slaveId].ALstatuscode));    \
 }
+
+int ELMOsetupGOLD(uint16 slave)
+{
+    int wkc = 0;
+    uint32_t sdoObj = 0x00000000;
+    uint8_t diable_bits = 0x00;
+    uint8_t enable_bits = 0x01;
+    uint16_t objAddr = 0x0000;
+    uint16_t TxAddr = 0x1607;
+    uint16_t RxAddr = 0x1A07;
+
+    wkc += ec_SDOwrite(slave, TxAddr, 0x00, FALSE, sizeof(diable_bits), &(diable_bits), EC_TIMEOUTSTATE); // 0 disable
+    sdoObj = 0x607A0020;                                                                                 // Target position
+    wkc += ec_SDOwrite(slave, TxAddr, 0x01, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60FF0020; // Target velocity
+    wkc += ec_SDOwrite(slave, TxAddr, 0x02, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60720010; // Max torque
+    wkc += ec_SDOwrite(slave, TxAddr, 0x03, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60400010; // Controlword
+    wkc += ec_SDOwrite(slave, TxAddr, 0x04, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60710010; // Target torque
+    wkc += ec_SDOwrite(slave, TxAddr, 0x05, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60600008; // Modes of operation
+    wkc += ec_SDOwrite(slave, TxAddr, 0x06, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    enable_bits = 0x06;
+    wkc += ec_SDOwrite(slave, TxAddr, 0x00, FALSE, sizeof(enable_bits), &(enable_bits), EC_TIMEOUTSTATE);
+
+    wkc += ec_SDOwrite(slave, RxAddr, 0x00, FALSE, sizeof(diable_bits), &(diable_bits), EC_TIMEOUTSTATE); // 0 disable
+    sdoObj = 0x60640020; // Position actual value
+    wkc += ec_SDOwrite(slave, RxAddr, 0x01, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60F40020; // Following error actual value
+    wkc += ec_SDOwrite(slave, RxAddr, 0x02, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60770010; // Torque actual value
+    wkc += ec_SDOwrite(slave, RxAddr, 0x03, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60410010; // Statusword
+    wkc += ec_SDOwrite(slave, RxAddr, 0x04, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x60610008; // Modes of operation display
+    wkc += ec_SDOwrite(slave, RxAddr, 0x05, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    sdoObj = 0x606C0020; // Additional position actual value
+    wkc += ec_SDOwrite(slave, RxAddr, 0x06, FALSE, sizeof(sdoObj), &(sdoObj), EC_TIMEOUTSTATE);
+    enable_bits = 0x06;
+    wkc += ec_SDOwrite(slave, RxAddr, 0x00, FALSE, sizeof(enable_bits), &(enable_bits), EC_TIMEOUTSTATE);
+
+   // Tx PDO disable
+    wkc += ec_SDOwrite(slave, 0x1c12, 0x00, FALSE, sizeof(diable_bits), &(diable_bits), EC_TIMEOUTSTATE);
+    // Tx PDO dictionary mapping
+    objAddr = TxAddr;
+    wkc += ec_SDOwrite(slave, 0x1c12, 0x01, FALSE, sizeof(objAddr), &(objAddr), EC_TIMEOUTSTATE);
+
+    // Rx PDO disable
+    wkc += ec_SDOwrite(slave, 0x1c13, 0x00, FALSE, sizeof(diable_bits), &(diable_bits), EC_TIMEOUTSTATE);
+    // Rx PDO dictionary mapping
+    objAddr = RxAddr;
+    wkc += ec_SDOwrite(slave, 0x1c13, 0x01, FALSE, sizeof(objAddr), &(objAddr), EC_TIMEOUTSTATE);
+
+    enable_bits = 0x01;
+    wkc += ec_SDOwrite(slave, 0x1c12, 0x00, FALSE, sizeof(enable_bits), &(enable_bits), EC_TIMEOUTSTATE);
+    wkc += ec_SDOwrite(slave, 0x1c13, 0x00, FALSE, sizeof(enable_bits), &(enable_bits), EC_TIMEOUTSTATE);
+
+
+    uint8 u8val = 8;                                                               // 8:position 9:velocity 10:torque
+    wkc += ec_SDOwrite(slave, 0x6060, 0x00, FALSE, sizeof(u8val), &u8val, EC_TIMEOUTSTATE); //    cyclic sychronous position mode
+    printf("wkc : %d\n", wkc);
+    printf("supported drive modes: %d\n", u8val);
+
+    return wkc;
+} 
+
+
 void simpletest(char *ifname,char *control_mode, float target_input, float max_velocity,float gear_ratio)
 {
    //build a struct TorqueOut pointer to the first output byte of the slave
@@ -142,10 +242,24 @@ void simpletest(char *ifname,char *control_mode, float target_input, float max_v
    {
       printf("ec_init on %s succeeded.\n",ifname);
       /* find and auto-config slaves */
-
-
-       if ( ec_config_init(FALSE) > 0 )
+      ec_slave[1].PO2SOconfig = &ELMOsetupGOLD;
+      if ( ec_config_init(FALSE) > 0 )
       {
+         // int32 ob2; 
+         // int os;
+        
+         // ec_dcsync0(0,TRUE,600000,0);
+         ec_configdc();
+         usleep(100000);
+         for (int i = 1; i <= ec_slavecount; i++) {
+            if (ELMOsetupGOLD(i)!=23)
+            {
+               printf("error \n");
+               exit(1);
+
+            };
+         }
+
          printf("%d slaves found and configured.\n",ec_slavecount);
 
          if (forceByteAlignment)
@@ -156,8 +270,10 @@ void simpletest(char *ifname,char *control_mode, float target_input, float max_v
          {
             ec_config_map(&IOmap);
          }
+         /* wait for all slaves to reach SAFE_OP state */
+         ec_statecheck(0, EC_STATE_PRE_OP,  EC_TIMEOUTSTATE * 4);
+        
 
-         ec_configdc();
          // show slave info
          for (int i = 1; i <= ec_slavecount; i++) {
                printf("\nSlave:%d\n Name:%s\n Output size: %dbits\n Input size: %dbits\n State: %d\n Delay: %d[ns]\n Has DC: %d\n",
@@ -165,19 +281,12 @@ void simpletest(char *ifname,char *control_mode, float target_input, float max_v
                   ec_slave[i].state, ec_slave[i].pdelay, ec_slave[i].hasdc);
          }
 
-            // /** disable heartbeat alarm */
-            // for (int i = 1; i <= ec_slavecount; i++) {
-            //     READ(i, 0x10F1, 2, buf32, "Heartbeat?");
-            //     WRITE(i, 0x10F1, 2, buf32, 1, "Heartbeat");
-
-            //     WRITE(i, 0x60c2, 1, buf8, 2, "Time period");
-            //     WRITE(i, 0x2f75, 0, buf16, 2, "Interpolation timeout");
-            // }
          printf("Slaves mapped, state to SAFE_OP.\n");
          /** set PDO mapping */
          /** opMode: 8  => csp */
          /** opMode: 9  => csv */
          for (int i = 1; i <= ec_slavecount; i++) {
+            READ(i, 0x1a03, 0, buf32, "rxPDO:0 1604");
             WRITE(i, 0x6060, 0, buf8, control_mode_uint, "OpMode");
             READ(i, 0x6061, 0, buf8, "OpMode display");
 
@@ -187,16 +296,19 @@ void simpletest(char *ifname,char *control_mode, float target_input, float max_v
 
             READ(i, 0x1c12, 1, buf32, "rxPDO:1");
             READ(i, 0x1c13, 1, buf32, "txPDO:1");
+
+
          }
 
-         int32 ob2; 
-         int os;
          for (int i = 1; i <= ec_slavecount; i++) {
-            os = sizeof(ob2); ob2 = 0x16040001;
-            ec_SDOwrite(i, 0x1c12, 0, TRUE, os, &ob2, EC_TIMEOUTRXM);
-            os = sizeof(ob2); ob2 = 0x1a030001;
-            ec_SDOwrite(i, 0x1c13, 0, TRUE, os, &ob2, EC_TIMEOUTRXM);
+            // os = sizeof(ob2); ob2 = 0x16040001;
+            // ec_SDOwrite(i, 0x1c12, 0, TRUE, os, &ob2, EC_TIMEOUTRXM);
+            // os = sizeof(ob2); ob2 = 0x1a070001;
+            // ec_SDOwrite(i, 0x1c13, 0, TRUE, os, &ob2, EC_TIMEOUTRXM);
+            
 
+            
+            READ(i, 0x1a07, 0, buf8, "txPDO:7");
             READ(i, 0x1c12, 0, buf32, "rxPDO:0");
             READ(i, 0x1c13, 0, buf32, "txPDO:0");
 
@@ -204,23 +316,23 @@ void simpletest(char *ifname,char *control_mode, float target_input, float max_v
             READ(i, 0x1c13, 1, buf32, "txPDO:1");
 
             READ(i, 0x1604, 0, buf32, "rxPDO:0 1604");
-            READ(i, 0x1a03, 0, buf32, "txPDO:0 1a04");
+            READ(i, 0x1a07, 0, buf32, "txPDO:0 0x1a07");
             READ(i, 0x1604, 1, buf32, "rxPDO:1 1604");
-            READ(i, 0x1a03, 1, buf32, "txPDO:1 1a04");
+            READ(i, 0x1a07, 1, buf32, "txPDO:1 0x1a07");
             READ(i, 0x1604, 2, buf32, "rxPDO:2 1604");
-            READ(i, 0x1a03, 2, buf32, "txPDO:2 1a04");
+            READ(i, 0x1a07, 2, buf32, "txPDO:2 0x1a07");
             READ(i, 0x1604, 3, buf32, "rxPDO:3 1604");
-            READ(i, 0x1a03, 3, buf32, "txPDO:3 1a04");
+            READ(i, 0x1a07, 3, buf32, "txPDO:3 0x1a07");
             READ(i, 0x1604, 4, buf32, "rxPDO:4 1604");
-            READ(i, 0x1a03, 4, buf32, "txPDO:4 1a04");
+            READ(i, 0x1a07, 4, buf32, "txPDO:4 0x1a07");
             READ(i, 0x6064, 0, sbuf32, "*position actual value*");
             WRITE(i, 0x6080, 0, buf32, max_velocity_uint, "*Max motor speed*"); usleep(100000);
             READ(i, 0x6080, 0, buf32, "*Max motor speed*");
          }
          /* wait for all slaves to reach SAFE_OP state */
          ec_statecheck(0, EC_STATE_SAFE_OP,  EC_TIMEOUTSTATE * 4);
-         target = (struct TorqueOut*)(ec_slave[1].outputs);
-         val = (struct TorqueIn*)(ec_slave[1].inputs);
+         target = (out_ELMOt*)(ec_slave[1].outputs);
+         val = (in_ELMOt*)(ec_slave[1].inputs);
          oloop = ec_slave[0].Obytes;
          if ((oloop == 0) && (ec_slave[0].Obits > 0)) oloop = 1;
          if (oloop > 8) oloop = 8;
@@ -292,7 +404,7 @@ void simpletest(char *ifname,char *control_mode, float target_input, float max_v
             // osal_thread_create(&thread2, 12800, &ecatprint,NULL);
             /* cyclic loop for two slaves*/
             target->control_word = 0;
-            target->maximal_torque = (uint16)(1000000);
+            target->max_torque = (uint16)(1000000);
             if (control_mode_uint == 8)
             {
                // READ(1, 0x6080, 0, buf32, "*max velocity*");
@@ -313,11 +425,12 @@ void simpletest(char *ifname,char *control_mode, float target_input, float max_v
                if (wkc >= expectedWKC) 
                {
                   printf("Processdata cycle %4d, WKC %d,", i, wkc);
-                  printf("pos: %f, vel: %f,pos err: %f, vel err: %f,  status: 0x%x, control: 0x%x", 
-                         (float)val->position_actual_value/gear_ratio * (2 * 3.1415) / 131071,
-                         (float)val->velocity_actual_value/gear_ratio * (2 * 3.1415) / 131071,
-                         (float)(target->target_position-val->position_actual_value)/gear_ratio * (2 * 3.1415) / 131071,
-                         (float)(target->target_velocity-val->velocity_actual_value)/gear_ratio * (2 * 3.1415) / 131071,
+                  printf("pos: %f, vel: %f, torque: %d pos err: %f, vel err: %f,  status: 0x%x, control: 0x%x", 
+                         (float)val->position_actual/gear_ratio * (2 * 3.1415) / 131071,
+                         (float)val->velocity_actual/gear_ratio * (2 * 3.1415) / 131071,
+                         val->torque_actual,
+                         (float)(target->target_position-val->position_actual)/gear_ratio * (2 * 3.1415) / 131071,
+                         (float)(target->target_velocity-val->velocity_actual)/gear_ratio * (2 * 3.1415) / 131071,
                            val->status_word, target->control_word);
                   
                   switch (target->control_word) {
